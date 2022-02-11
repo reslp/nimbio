c7d00498f88b1a458435abe0f5bf6666104a6dbd
Philipp Resl <resl.philipp@gmail.com>
2022-02-11 11:55:34 +0100

    Make test read fasta work again

--- tests/test_read_fasta.nim
+++ tests/test_read_fasta.nim
@@ -1,24 +1,18 @@
-import nimbio/src/nimbio/SeqIO
+import src/nimbio.FastaIO
+
 # this is where the file is opened
-let input = open("sequences.fasta")
+let input = open("tests/test_data/sequences.fasta")
 var i = 0
 for sequence in ReadFasta(input):
      echo "Original sequence:"
      echo sequence.toString()
      echo "Sequence as upper case:"
-     echo sequence.toUpper().toString()
+     echo sequence.toUpper.toString
      echo "Sequence as lower case"
      echo sequence.toLower().toString()
      echo "Reverse Complement of sequence"
      echo sequence.reverse_complement().toString()
 
+import nimbio/FastQIO
 echo("Check FASTQ handling")
-let input2 = open("testdata/subset.fastq")
-var readall: FQSeqRecordArray = ReadFastQ(input2)
-check readall.len == 1000
 
-echo("Check printing of FASTQ")
-echo("Single sequence")
-echo(readall[1])
-echo("Whole array")
-echo(readall)
