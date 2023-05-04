081db7d705a3b7f04e83e612a9cea12ce1a71b3d
Philipp Resl <resl.philipp@gmail.com>
2023-05-04 20:36:37 +0200

    Fix indentation in readfasta test

--- tests/test_read_fasta.nim
+++ tests/test_read_fasta.nim
@@ -7,13 +7,15 @@ suite "Test FASTA handling":
   let input = open("tests/test_data/sequences.fasta")
   var i = 0
   for sequence in ReadFasta(input):
-       echo "Original sequence:"
-       echo sequence.toString()
-       echo "Sequence as upper case:"
-       echo sequence.toUpper.toString
-       echo "Sequence as lower case"
-       echo sequence.toLower().toString()
-       echo "Reverse Complement of sequence"
-       echo sequence.reverse_complement().toString()
+    echo "Original sequence:"
+    echo sequence.toString()
+    echo "Sequence as upper case:"
+    echo sequence.toUpper.toString
+    echo "Sequence as lower case"
+    echo sequence.toLower().toString()
+    echo "Reverse Complement of sequence"
+    echo sequence.reverse_complement().toString()
+    echo(sequence)
+
 
 
